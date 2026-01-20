baf23f17d27b7d4b39d532584a37043a3d1f6165
ViitoDev <joaovictorpiresdev@gmail.com>
2026-01-20 09:43:06 -0300

    REFACTOR - Melhora da formatação

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ print("3. Ativar restaurante")
 print("4. Sair\n")
 
 opcao_escolhida = input("Escolha uma opção: ")
-print("Você escolheu a opção", opcao_escolhida)
+print(f"Você escolheu a opção {opcao_escolhida}")
