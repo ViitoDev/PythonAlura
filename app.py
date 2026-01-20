70e4f57cf6ab9aa0a1da8544488f7345280f5060
ViitoDev <joaovictorpiresdev@gmail.com>
2026-01-20 09:41:56 -0300

    FEAT - Escolha da opção

--- app.py
+++ app.py
@@ -2,4 +2,7 @@ print("Delivery App\n")
 print("1. Cadastrar restaurante")
 print("2. Listar resturante")
 print("3. Ativar restaurante")
-print("4. Sair")
+print("4. Sair\n")
+
+opcao_escolhida = input("Escolha uma opção: ")
+print("Você escolheu a opção", opcao_escolhida)
